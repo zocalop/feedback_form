393fe99005e3d4cabf4868898600836af733b997
zocalop <jcb2861@gmail.com>
2026-05-19 21:56:19 -0300

    Updated FeedbackForm.jsx in main

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -11,7 +11,21 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+        <textarea
+          name="feedback"
+          placeholder="Your feedback"
+        />        
+        <button type="submit">Submit feedback</button>
       </form>
     </>
   );
